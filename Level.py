27871b31e93c4a87e3602355708e9bc91acb55e3
i_chowdhury18 <i_chowdhury18@cranford.hounslow.sch.uk>
2024-04-24 12:45:44 +0100

    editing

--- Level.py
+++ Level.py
@@ -1,9 +1,9 @@
 import pygame.display
-
 from Settings import *
 
+
 class Level1:
-    def __init__(self):
+    def __init__(self, tmx_map):
         self.display_surface = pygame.display.get_surface()
 
     def run(self):
--- Main.py
+++ Main.py
@@ -1,23 +1,31 @@
+import pygame.time
+
 from Settings import *
 from Level import Level1
+from pytmx.util_pygame import load_pygame
+from os.path import join
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Super Star Quest")
+        self.clock = pygame.time.Clock()
+
+        self.tmx_maps = {0: load_pygame(join('..', 'data', 'levels', 'omni.tmx'))}
+        self.current_stage = Level(self.tmx_maps[0])
 
         self.current_stage = Level1()
 
 
     def run(self):
         while True:
+            dt= self.clock.tick() / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-            self.current_stage.run()
-
+            self.current_stage.run(dt)
             pygame.display.update()
 
 if __name__=='__main__':
