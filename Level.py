275dc2662298d02993a4f308621fedbda2b40efb
i_chowdhury18 <i_chowdhury18@cranford.hounslow.sch.uk>
2024-04-25 13:48:02 +0100

    new files

--- Level.py
+++ Level.py
@@ -6,5 +6,13 @@ class Level1:
     def __init__(self, tmx_map):
         self.display_surface = pygame.display.get_surface()
 
+
+    def setup(self, tmx_map):
+        for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            print(x)
+            print(y)
+            print(surf)
+
+
     def run(self):
         self.display_surface.fill('blue')
